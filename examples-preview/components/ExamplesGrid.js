37173e8ddda28daa9b630092e9dcfe61c3124db0
Zain🍕 <zainadam.id@gmail.com>
2024-12-21 22:55:14 +0800

    Refactor ExamplesGrid component to use functional component and hooks; improve search functionality and update styling

--- examples-preview/components/ExamplesGrid.js
+++ examples-preview/components/ExamplesGrid.js
@@ -1,88 +1,91 @@
-import {Link} from "react-router-dom";
-import {Card, CardContent, Grid, Typography} from "@mui/material";
-import React from "react";
-import examples from "./examples";
-import { withStyles } from "tss-react/mui";
-import TextField from '@mui/material/TextField';
+import { Box, Card, CardActionArea, CardContent, Grid, TextField, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { withStyles } from 'tss-react/mui';
+// locals
+import examples from './examples';
 
-const styles = {
-    container: {
-        flexGrow: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 16,
-    },
-    card: {
-        '&:hover': {
-            background: 'lightgrey',
-            fontWeight: 500,
-        }
-    },
-    cardContent: {
-        '&:last-child': {
-            padding: 8,
-        }
-    },
-    link: {
-        textDecoration: 'none',
-    },
-    label: {
-        fontWeight: 'inherit'
-    }
-};
+const EXAMPLE_SORTED = {};
 
-class ExamplesGrid extends React.Component {
+/** Sort Examples alphabetically */
+Object.keys(examples)
+  .sort()
+  .forEach(function(key) {
+    EXAMPLE_SORTED[key] = examples[key];
+  });
 
-  state = {
-    searchVal: ''
-  }
+function ExamplesGrid({ classes }) {
+  const [searchVal, setSearchVal] = useState('');
 
-  setSearchVal = (val) => {
-    this.setState({
-      searchVal: val
-    });
-  }
+  const examplesSortedKeys = Object.keys(EXAMPLE_SORTED).filter(item => {
+    if (searchVal === '') return true;
+    console.dir(item);
+    return item.toLowerCase().indexOf(searchVal.toLowerCase()) !== -1 ? true : false;
+  });
 
-  render() {
-    const {classes} = this.props;
+  return (
+    <>
+      <SearchBar
+        nItems={Object.keys(examplesSortedKeys).length}
+        value={searchVal}
+        onChange={({ target: { value } }) => setSearchVal(value)}
+      />
 
-    // Sort Examples alphabetically
-    const examplesSorted = {};
-    Object.keys(examples).sort().forEach(function (key) {
-        examplesSorted[key] = examples[key];
-    });
-
-    const examplesSortedKeys = Object.keys(examplesSorted).filter((item) => {
-      if (this.state.searchVal === '') return true;
-      console.dir(item);
-      return item.toLowerCase().indexOf( this.state.searchVal.toLowerCase() ) !== -1 ? true : false;
-    });
-
-    return (
-      <React.Fragment>
-        <Typography variant="h5" align="center">Choose an Example</Typography>
-        <Typography variant="subtitle2" align="center">({examplesSortedKeys.length}) Examples</Typography>
-
-        <Typography variant="subtitle2" align="center" style={{margin:'10px'}}>
-          <TextField placeholder="Search Examples" value={this.state.searchVal} onChange={(e) => this.setSearchVal(e.target.value)} />
-        </Typography>
-
-        <Grid container className={classes.container} spacing={1}>
-          {examplesSortedKeys.map((label, index) => (
-            <Grid key={index} item md={2}>
-              <Link className={classes.link} to={`/${label.replace(/\s+/g, '-').toLowerCase()}`}>
-                <Card className={classes.card}>
-                  <CardContent className={classes.cardContent}>
-                    <Typography variant="subtitle1" className={classes.label} align="center">{label}</Typography>
-                  </CardContent>
-                </Card>
-              </Link>
-            </Grid>
-          ))}
-        </Grid>
-      </React.Fragment>
-    );
-  }
+      <Grid
+        container
+        spacing={1}
+        sx={{
+          mt: 4,
+        }}>
+        {examplesSortedKeys.map((label, i) => (
+          <Grid key={i} item md={2}>
+            <Card className={classes.card}>
+              <CardActionArea component={Link} to={`/${label.replace(/\s+/g, '-').toLowerCase()}`}>
+                <CardContent className={classes.cardContent}>
+                  <Typography variant="subtitle1" className={classes.label} align="center">
+                    {label}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </>
+  );
 }
 
-export default withStyles(ExamplesGrid, styles);
+const STYLES = {
+  card: {
+    '&:hover': {
+      background: 'lightgrey',
+      fontWeight: 500,
+    },
+  },
+  cardContent: {
+    '&:last-child': {
+      padding: 8,
+    },
+  },
+  label: {
+    fontWeight: 'inherit',
+  },
+};
+
+export default withStyles(ExamplesGrid, STYLES);
+
+function SearchBar({ nItems, onChange, value }) {
+  return (
+    <Box>
+      <Typography variant="h5" component="div">
+        Choose an example
+      </Typography>
+
+      <Typography variant="subtitle2" component="div" mb={1}>
+        ({nItems}) examples
+      </Typography>
+
+      <TextField placeholder="Search Examples" value={value} onChange={onChange} />
+    </Box>
+  );
+}
--- examples-preview/index.js
+++ examples-preview/index.js
@@ -1,74 +1,68 @@
-import React from 'react';
+import { Box, Button, Container } from '@mui/material';
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router, Route, Switch, withRouter } from 'react-router-dom';
-import { withStyles } from 'tss-react/mui';
-import ExamplesGrid from './components/ExamplesGrid';
+import { HashRouter, Link, Route, Switch } from 'react-router-dom';
+import { Home } from '@mui/icons-material';
+// locals
 import examples from './components/examples';
-import Button from '@mui/material/Button';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ExamplesGrid from './components/ExamplesGrid';
 
-const styles = {
-  root: {
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  contentWrapper: {
-    width: '100%',
-  },
-};
+/** Render the App */
+createRoot(document.getElementById('app-root')).render(<App />);
 
-class Examples extends React.Component {
-  returnHome = () => {
-    this.props.history.push('/');
-  };
-
-  render() {
-    const { classes } = this.props;
-
-    var returnHomeStyle = { padding: '0px', margin: '20px 0 20px 0' };
-
-    const defaultTheme = createTheme();
-
-    return (
-      <ThemeProvider theme={defaultTheme}>
-        <main className={classes.root}>
-          <div className={classes.contentWrapper}>
-            <Switch>
-              <Route path="/" exact render={() => <ExamplesGrid examples={examples} />} />
-              {Object.keys(examples).map((label, index) => (
-                <Route
-                  key={index}
-                  path={`/${label.replace(/\s+/g, '-').toLowerCase()}`}
-                  exact
-                  component={examples[label]}
-                />
-              ))}
-            </Switch>
-            <div>
-              {this.props.location.pathname !== '/' && (
-                <div style={returnHomeStyle}>
-                  <Button color="primary" onClick={this.returnHome}>
-                    Back to Example Index
-                  </Button>
-                </div>
-              )}
-            </div>
-          </div>
-        </main>
-      </ThemeProvider>
-    );
-  }
-}
-
-const StyledExamples = withRouter(withStyles(Examples, styles));
-
+/**
+ * App Component
+ */
 function App() {
   return (
-    <Router hashType="noslash">
-      <StyledExamples />
-    </Router>
+    <Providers>
+      <Layout>
+        <ContentSwitcher />
+      </Layout>
+    </Providers>
   );
 }
-const container = document.getElementById('app-root');
-const root = createRoot(container);
-root.render(<App />);
+
+/**
+ * Providers Component
+ *
+ * @param {ReactNode} props.children
+ */
+function Providers({ children }) {
+  return <HashRouter hashType="noslash">{children}</HashRouter>;
+}
+
+/**
+ * Layout Component
+ */
+function Layout({ children }) {
+  return (
+    <Container
+      maxWidth="md"
+      sx={{
+        mb: 16,
+      }}>
+      <Button component={Link} to="/" startIcon={<Home />} sx={{ mb: 2 }}>
+        Home
+      </Button>
+
+      <Box component="main" sx={{ mt: 4 }}>
+        {children}
+      </Box>
+    </Container>
+  );
+}
+
+/**
+ * ContentSwitcher Component
+ */
+function ContentSwitcher() {
+  return (
+    <Switch>
+      <Route path="/" exact render={() => <ExamplesGrid examples={examples} />} />
+
+      {Object.keys(examples).map((label, index) => (
+        <Route key={index} path={`/${label.replace(/\s+/g, '-').toLowerCase()}`} exact component={examples[label]} />
+      ))}
+    </Switch>
+  );
+}
