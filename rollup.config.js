694ef98cef0ac6d8cc413d3be8ca10d90298f200
Zain🍕 <zainadam.id@gmail.com>
2024-12-24 00:54:07 +0800

    Update Rollup configuration and it is dependencies for TypeScript support and improved build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,52 @@
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import filesize from 'rollup-plugin-filesize'
-import replace from '@rollup/plugin-replace'
+import nodeResolve from '@rollup/plugin-node-resolve'
 import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
 
-import pkg from './package.json'
+import pkg from './package.json' assert { type: 'json' }
 
+/** @type {import('rollup').RollupOptions['plugins']} */
 const PLUGINS = [
-    replace({
-        'process.env.NODE_ENV': JSON.stringify('production')
-    }),
+    /** Locate modules using the Node resolution algorithm, for using third party modules in node_modules */
+    nodeResolve(),
+
+    /** Convert CommonJS modules to ES6, so they can be included in a Rollup bundle */
     commonjs({
         include: ['node_modules/**']
     }),
+
+    /** To wide-support browser */
     babel({
         babelHelpers: 'runtime',
         babelrc: true
     }),
+
+    /** To minify the bundled files */
     terser(),
-    filesize()
+
+    /** To show the bundled size */
+    filesize(),
+
+    /** To bundling ts/tsx files */
+    typescript({
+        importHelpers: true,
+        tsconfig: './tsconfig.app.json',
+        rootDir: './src',
+
+        compilerOptions: {
+            declaration: true,
+            declarationDir: './dist',
+            isolatedDeclarations: true
+        }
+    })
 ]
 
+/** @type {import('rollup').RollupOptions} */
 export default {
-    input: 'src/index.js',
+    external: [/node_modules/],
+    input: 'src/index.ts',
     plugins: PLUGINS,
     output: [
         {
@@ -34,7 +58,8 @@ export default {
         {
             file: pkg.module,
             format: 'es',
-            sourcemap: true
+            sourcemap: true,
+            exports: 'named'
         }
     ]
 }
