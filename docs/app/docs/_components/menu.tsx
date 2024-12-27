10e58ff757ca1517d07804def578d0e5d9ce9af7
Zain🍕 <zainadam.id@gmail.com>
2024-12-28 04:22:20 +0800

    Refactor Menu component to include a "Getting Started" section and update SandboxItem links to use Next.js Link component

--- docs/app/docs/_components/menu.tsx
+++ docs/app/docs/_components/menu.tsx
@@ -6,6 +6,7 @@ import {
     ListItemText,
     ListSubheader
 } from '@mui/material'
+import Link from 'next/link'
 
 const drawerWidth = 240
 
@@ -31,10 +32,13 @@ export default function Menu({
                 }
             }}
         >
-            <List
-                component="nav"
-                subheader={<ListSubheader>Examples</ListSubheader>}
-            >
+            <List component="nav">
+                <ListSubheader>Getting Started</ListSubheader>
+                <SandboxItem
+                    href="/docs/getting-started/overview"
+                    name="Overview"
+                />
+
                 {sandboxes.map((item, i) => (
                     <SandboxItem key={i} href={item.href} name={item.name} />
                 ))}
@@ -44,44 +48,39 @@ export default function Menu({
 }
 
 const sandboxes = [
-    {
-        name: 'Custom Component',
-        href: 'https://codesandbox.io/embed/xrvrzryjvp?autoresize=1&hidenavigation=1'
-    },
-    {
-        name: 'Customize Columns',
-        href: 'https://codesandbox.io/embed/xowj5oj8w?autoresize=1&hidenavigation=1'
-    },
-    {
-        name: 'Customize Footer',
-        href: 'https://codesandbox.io/embed/5z0w0w9jyk?autoresize=1&hidenavigation=1'
-    },
-    {
-        name: 'Customize Styling',
-        href: 'https://codesandbox.io/embed/0ylq1lqwp0?autoresize=1&hidenavigation=1'
-    },
-    {
-        name: 'Customize Toolbar',
-        href: 'https://codesandbox.io/embed/wy2rl1nyzl?autoresize=1&hidenavigation=1'
-    },
-    {
-        name: 'Customize ToolbarSelect',
-        href: 'https://codesandbox.io/embed/545ym5ov6p?autoresize=1&hidenavigation=1'
-    },
-    {
-        name: 'Resizable Columns',
-        href: 'https://codesandbox.io/embed/q8w3230qpj?autoresize=1&hidenavigation=1'
-    }
+    // {
+    //     name: 'Custom Component',
+    //     href: 'https://codesandbox.io/embed/xrvrzryjvp?autoresize=1&hidenavigation=1'
+    // },
+    // {
+    //     name: 'Customize Columns',
+    //     href: 'https://codesandbox.io/embed/xowj5oj8w?autoresize=1&hidenavigation=1'
+    // },
+    // {
+    //     name: 'Customize Footer',
+    //     href: 'https://codesandbox.io/embed/5z0w0w9jyk?autoresize=1&hidenavigation=1'
+    // },
+    // {
+    //     name: 'Customize Styling',
+    //     href: 'https://codesandbox.io/embed/0ylq1lqwp0?autoresize=1&hidenavigation=1'
+    // },
+    // {
+    //     name: 'Customize Toolbar',
+    //     href: 'https://codesandbox.io/embed/wy2rl1nyzl?autoresize=1&hidenavigation=1'
+    // },
+    // {
+    //     name: 'Customize ToolbarSelect',
+    //     href: 'https://codesandbox.io/embed/545ym5ov6p?autoresize=1&hidenavigation=1'
+    // },
+    // {
+    //     name: 'Resizable Columns',
+    //     href: 'https://codesandbox.io/embed/q8w3230qpj?autoresize=1&hidenavigation=1'
+    // }
 ]
 
 const SandboxItem = ({ href, name }: { href: string; name: string }) => (
     <ListItem disablePadding>
-        <ListItemButton
-            onClick={() => window.open(href, '_blank')}
-            sx={{
-                py: 0
-            }}
-        >
+        <ListItemButton href={href} LinkComponent={Link}>
             <ListItemText primary={name} />
         </ListItemButton>
     </ListItem>
