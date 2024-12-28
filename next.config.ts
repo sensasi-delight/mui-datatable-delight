18b45c9095f8193a7d33637058add80cbc71733d
Zain🍕 <zainadam.id@gmail.com>
2024-12-28 20:03:33 +0800

    Add TypeScript configuration to ignore build errors during migration

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import createMDX from '@next/mdx'
 
 const nextConfig: NextConfig = {
     pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
-    reactStrictMode: true
+    reactStrictMode: true,
+
+    typescript: {
+        /**
+         * WILL REMOVE THIS AFTER ALL CODES ARE FULLY MIGRATE TO TYPESCRIPT
+         */
+        ignoreBuildErrors: true
+    }
 }
 
 const withMDX = createMDX()
