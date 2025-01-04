83687d270c481c377f4c21326287d0401565d543
Zain🍕 <zainadam.id@gmail.com>
2025-01-04 20:58:20 +0800

    Add `DataTableIcons` interface and update `DEFAULT_ICONS` for improved icon management

--- src/hooks/use-main-context.default-icons.ts
+++ src/hooks/use-main-context.default-icons.ts
@@ -5,8 +5,9 @@ import {
     Search as SearchIcon,
     ViewColumn as ViewColumnIcon
 } from '@mui/icons-material'
+import { DataTableIcons } from '../data-table.props.type/icons'
 
-export const DEFAULT_ICONS = {
+export const DEFAULT_ICONS: DataTableIcons = {
     DownloadIcon,
     FilterIcon,
     PrintIcon,
