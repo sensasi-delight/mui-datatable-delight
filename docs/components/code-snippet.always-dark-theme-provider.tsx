61ad1baf8486e21f4e752a14f88ff2fe8859794c
Adam Akbar <zainadam.id@gmail.com>
2025-01-10 01:38:37 +0800

    split dark theme provider component to it's own file (#87)

---
+++ docs/components/code-snippet.always-dark-theme-provider.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { createTheme, ThemeProvider } from '@mui/material'
+import type { ReactNode } from 'react'
+
+export function AlwaysDarkThemeProvider({ children }: { children: ReactNode }) {
+    return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
+
+const theme = createTheme({
+    colorSchemes: {
+        dark: true
+    },
+    palette: {
+        mode: 'dark'
+    }
+})
