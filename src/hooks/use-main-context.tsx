2953db39c4f59ade680a13781f613c9cbabae976
Zain🍕 <zainadam.id@gmail.com>
2025-01-12 07:32:54 +0800

    Enhance state management in MainContext by integrating restored state and initializing options, while adding validation for table options

--- src/hooks/use-main-context.tsx
+++ src/hooks/use-main-context.tsx
@@ -1,7 +1,13 @@
 // vendors
-import { createContext, ReactNode, useContext, useState } from 'react'
+import {
+    createContext,
+    ReactNode,
+    useContext,
+    useEffect,
+    useState
+} from 'react'
 // local types
-import type { DataTableProps } from '..'
+import type { DataTableOptions, DataTableProps } from '..'
 // functions
 import { processTextLabels } from './use-main-context.process-text-labels'
 // defaults
@@ -9,7 +15,7 @@ import { DEFAULT_ICONS } from './use-main-context.default-icons'
 import { DEFAULT_COMPONENTS } from './use-main-context.default-components'
 import { DataTableState } from '../data-table.props.type/state'
 import { TableAction } from '../data-table.props.type/options'
-import { save } from '../functions'
+import { getNewStateOnDataChange, load, save, warnInfo } from '../functions'
 
 export function MainContextProvider({
     datatableProps,
@@ -18,16 +24,47 @@ export function MainContextProvider({
     datatableProps: DataTableProps
     children: ReactNode
 }): ReactNode {
-    const [state, setState] = useState(DEFAULT_STATE)
+    const restoredState = datatableProps.options?.storageKey
+        ? load(datatableProps.options.storageKey)
+        : undefined
 
-    function onStateChange(action: TableAction, state: DataTableState) {
-        setState(state)
+    const [state, setState] = useState(
+        getNewStateOnDataChange(
+            datatableProps,
+            1,
+            true,
+            datatableProps.options ?? {},
+            {
+                ...DEFAULT_STATE,
+                ...getInitTableOptions(datatableProps.options),
+                ...restoredState
+            },
+            () => {}
+        )
+    )
 
-        datatableProps.options?.onTableChange?.(action, state)
+    function onStateChange(
+        action: TableAction,
+        newPartialState: Partial<DataTableState>
+    ) {
+        setState(prev => {
+            const newState = {
+                ...prev,
+                ...newPartialState
+            }
 
-        if (datatableProps.options?.storageKey)
-            save(datatableProps.options.storageKey, state)
+            datatableProps.options?.onTableChange?.(action, newState)
+
+            if (datatableProps.options?.storageKey)
+                save(datatableProps.options.storageKey, newState)
+
+            return newState
+        })
     }
+
+    useEffect(() => {
+        datatableProps.options?.onTableInit?.(TableAction.INITIALIZED, state)
+    }, [])
 
     return (
         <MainContext.Provider
@@ -98,4 +135,63 @@ interface ContextValueType {
     onStateChange?: (action: TableAction, state: DataTableState) => void
     state: typeof DEFAULT_STATE
     textLabels: ReturnType<typeof processTextLabels>
+}
+
+function getInitTableOptions({
+    rowsPerPage,
+    page,
+    rowsSelected,
+    rowsPerPageOptions
+}: DataTableProps['options'] = {}) {
+    const optState: {
+        page?: DataTableState['page']
+        rowsPerPage?: DataTableState['rowsPerPage']
+        rowsPerPageOptions?: DataTableState['rowsPerPageOptions']
+        rowsSelected?: DataTableState['rowsSelected']
+    } = {}
+
+    if (rowsPerPage) {
+        optState.rowsPerPage = rowsPerPage
+    }
+
+    if (page) {
+        optState.page = page
+    }
+
+    if (rowsSelected) {
+        optState.rowsSelected = rowsSelected
+    }
+
+    if (rowsPerPageOptions) {
+        optState.rowsPerPageOptions = rowsPerPageOptions
+    }
+
+    function validateOptions(options: DataTableOptions) {
+        if (options.serverSide && options.onTableChange === undefined) {
+            throw Error(
+                'onTableChange callback must be provided when using serverSide option'
+            )
+        }
+        if (
+            options.expandableRows &&
+            options.renderExpandableRow === undefined
+        ) {
+            throw Error(
+                'renderExpandableRow must be provided when using expandableRows option'
+            )
+        }
+        if (
+            options.rowsSelected &&
+            Array.isArray(options.rowsSelected) &&
+            options.rowsSelected.some(isNaN)
+        ) {
+            warnInfo(
+                'When using the rowsSelected option, must be provided an array of numbers only.'
+            )
+        }
+    }
+
+    validateOptions(optState)
+
+    return optState
 }
