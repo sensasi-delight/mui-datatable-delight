f49e4e424166ee8c39eecdb84ebd0749d7ef936d
Zain🍕 <zainadam.id@gmail.com>
2025-01-21 03:50:36 +0800

    Change the types builder from `rollup` to `tsc` and refactor

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,6 +7,9 @@ import typescript from '@rollup/plugin-typescript'
 
 /** @type {import('rollup').RollupOptions['plugins']} */
 const PLUGINS = [
+    /** To delete `dist` dir before build */
+    del({ targets: 'dist/*' }),
+
     /** Locate modules using the Node resolution algorithm, for using third party modules in node_modules */
     nodeResolve(),
 
@@ -17,33 +20,23 @@ const PLUGINS = [
     terser(),
 
     /** To preserve `use client` directive */
-    preserveDirectives()
+    preserveDirectives(),
+
+    /** To build from tsx files */
+    typescript({
+        compilerOptions: {
+            allowImportingTsExtensions: false
+        }
+    })
 ]
 
-/** @type {import('@rollup/plugin-typescript').RollupTypescriptOptions} */
-const TS_OPTS_FOR_ESM_BUILD = {
-    tsconfig: './tsconfig.json',
-    rootDir: './src',
-
-    compilerOptions: {
-        allowImportingTsExtensions: false
-    }
-}
-
-/** @type {import('@rollup/plugin-typescript').RollupTypescriptOptions} */
-const TS_OPTS_FOR_CJS_BUILD = {
-    ...TS_OPTS_FOR_ESM_BUILD,
-
-    compilerOptions: {
-        allowImportingTsExtensions: false,
-        declaration: true,
-        declarationDir: 'dist/cjs',
-        emitDeclarationOnly: true
-    }
-}
-
-/** @type {import('rollup').RollupOptions} */
-const ROLLUP_BASE_OPTS = {
+/** @type {import('rollup').RollupOptions[]} */
+export default {
+    /**
+     * @todo REMOVE THIS TO DECREASE BUNDLE SIZE
+     * @todo ALSO DO NOT FORGET TO REMOVE `redux` THAT ONLY REQUIRED BY `react-dnd`
+     * @todo ALSO DO NOT FORGET TO REMOVE `src/` IN `main` AND `module` IN PACKAGE JSON
+     */
     external: id => {
         if (id.includes('dnd-core') || id.includes('react-dnd')) {
             return false
@@ -51,37 +44,21 @@ const ROLLUP_BASE_OPTS = {
 
         return /node_modules/.test(id)
     },
-    input: 'src/index.ts'
-}
-
-/** @type {import('rollup').RollupOptions[]} */
-export default [
-    {
-        ...ROLLUP_BASE_OPTS,
-        output: {
+    input: 'src/index.ts',
+    output: [
+        {
             dir: 'dist/cjs',
-            exports: 'named',
             format: 'cjs',
+            exports: 'named',
             preserveModules: true,
             sourcemap: true
         },
-        plugins: [
-            /** To delete `dist` dir before build */
-            del({ targets: 'dist/*' }),
-
-            ...PLUGINS,
-
-            typescript(TS_OPTS_FOR_CJS_BUILD)
-        ]
-    },
-    {
-        ...ROLLUP_BASE_OPTS,
-        output: {
+        {
             dir: 'dist/esm',
             format: 'es',
             preserveModules: true,
             sourcemap: true
-        },
-        plugins: [...PLUGINS, typescript(TS_OPTS_FOR_ESM_BUILD)]
-    }
-]
+        }
+    ],
+    plugins: PLUGINS
+}
