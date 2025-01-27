f7d5ac88711e3fc74167aafed351f52fd35ee21f
Zain🍕 <zainadam.id@gmail.com>
2025-01-28 03:47:52 +0800

    Resolve #124

--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -1,9 +1,11 @@
 import type { MDXComponents } from 'mdx/types'
 import Alert from '@mui/material/Alert'
 import Link from '@mui/material/Link'
-import Typography from '@mui/material/Typography'
+import LinkIcon from '@mui/icons-material/Link'
+import Typography, { type TypographyProps } from '@mui/material/Typography'
 import { CodeSnippet, InlineCode } from './components'
 import { Mermaid } from './app/(main)/_components/mermaid'
+import type { JSX } from 'react'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
@@ -27,15 +29,16 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
             </Typography>
         ),
         h2: ({ children }) => (
-            <Typography variant="h4" component="h2" pt={1} my={3}>
+            <HeadingLink variant="h4" component="h2">
                 {children}
-            </Typography>
+            </HeadingLink>
         ),
         h3: ({ children }) => (
-            <Typography variant="h5" component="h3" pt={1} my={3}>
+            <HeadingLink variant="h5" component="h3">
                 {children}
-            </Typography>
+            </HeadingLink>
         ),
+
         blockquote: ({ children }) => (
             <Alert
                 component="blockquote"
@@ -62,4 +65,73 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
         ),
         ...components
     }
+}
+
+function HeadingLink({
+    children,
+    variant,
+    component
+}: {
+    children: JSX.Element
+    variant: TypographyProps['variant']
+    component: Exclude<TypographyProps['component'], undefined>
+}) {
+    const text = toText(children)
+
+    return (
+        <Link
+            href={`#${text}`}
+            display="flex"
+            alignItems="baseline"
+            gap={1}
+            sx={{
+                textDecoration: 'none',
+                color: 'inherit',
+                pt: 1,
+                my: 3,
+                wordBreak: 'break-all',
+
+                ':hover': {
+                    '& svg': {
+                        display: 'block'
+                    }
+                }
+            }}
+        >
+            <Typography
+                id={text}
+                variant={variant}
+                component={component}
+                sx={{
+                    scrollMarginTop: '96px'
+                }}
+            >
+                {children}
+            </Typography>
+
+            <LinkIcon
+                sx={{
+                    display: 'none',
+                    color: 'primary.main',
+                    transform: 'translateY(4px)'
+                }}
+            />
+        </Link>
+    )
+}
+
+function toText(children: JSX.Element): string {
+    return (
+        typeof children === 'string'
+            ? children
+            : children.props &&
+                typeof children.props === 'object' &&
+                'children' in children.props
+              ? (children.props.children as string)
+              : ''
+    )
+        .toLowerCase()
+        .replace(/[^0-9A-Z ]+/gi, '')
+        .trim()
+        .replaceAll(' ', '-')
 }
