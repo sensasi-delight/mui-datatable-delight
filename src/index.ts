7b90c97ce4dd75fd5a88d178c355558ce9985098
Zain🍕 <zainadam.id@gmail.com>
2025-02-06 17:55:31 +0800

    Release `v0.0.0-experimental.34` to text export default simple component

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
-export type { DataTableProps } from './data-table.props'
-export type { DataTableOptions } from './types/options'
-export type { DataTableState } from './types/state'
+// export type { DataTableProps } from './data-table.props'
+// export type { DataTableOptions } from './types/options'
+// export type { DataTableState } from './types/state'
 
-export { DataTable as default } from './data-table'
+export { DataTable } from './data-table'
+export { default } from './simple-h1'
 
 // ########## CONTEXT ##########
-export { default as useDataTableContext } from './hooks/use-data-table-context'
-export { default as DataTableContextProvider } from './hooks/use-data-table-context/components/provider'
+// export { default as useDataTableContext } from './hooks/use-data-table-context'
+// export { default as DataTableContextProvider } from './hooks/use-data-table-context/components/provider'
 
-// ########## <Datatable /> ##########
-export { default as AnnounceText } from './components/announce-text'
+// // ########## <Datatable /> ##########
+// export { default as AnnounceText } from './components/announce-text'
 
-export { default as BottomBar } from './components/bottom-bar'
+// export { default as BottomBar } from './components/bottom-bar'
 
-export { default as ColumnsResizer } from './components/columns-resizer'
+// export { default as ColumnsResizer } from './components/columns-resizer'
 
-export { default as FilteredValuesList } from './components/filtered-values-list'
+// export { default as FilteredValuesList } from './components/filtered-values-list'
 
-export { default as SelectedRowsToolbar } from './components/selected-rows-toolbar'
+// export { default as SelectedRowsToolbar } from './components/selected-rows-toolbar'
 
-// ########## <Table /> ##########
-export { default as Table } from './components/table'
-export { default as RowExpansionButton } from './components/table/components/_shared/checkbox-cell/components/row-expansion-button'
-export { default as TableBody } from './components/table/components/body'
-export { default as TableHead } from './components/table/components/head'
+// // ########## <Table /> ##########
+// export { default as Table } from './components/table'
+// export { default as RowExpansionButton } from './components/table/components/_shared/checkbox-cell/components/row-expansion-button'
+// export { default as TableBody } from './components/table/components/body'
+// export { default as TableHead } from './components/table/components/head'
 
-// ########## <Toolbar /> ##########
-export { default as Toolbar } from './components/toolbar'
-export { default as DataFilterBox } from './components/toolbar/components/data-filter-box'
-export { default as ColumnVisibilitiesBox } from './components/toolbar/components/column-visibilities-box'
+// // ########## <Toolbar /> ##########
+// export { default as Toolbar } from './components/toolbar'
+// export { default as DataFilterBox } from './components/toolbar/components/data-filter-box'
+// export { default as ColumnVisibilitiesBox } from './components/toolbar/components/column-visibilities-box'
---
+++ src/simple-h1.tsx
@@ -0,0 +1,6 @@
+/**
+ * A React functional component that renders a simple H1 element for testing purposes.
+ */
+export default function SimpleH1() {
+    return <h1>Simple H1 for test</h1>
+}
